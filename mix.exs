4d7d2ec006cc29c350e7bf4415c6ba10cf888bdb
Luzian Wild <luzian.wild@gmail.com>
2024-07-12 17:12:37 +0200

    Elixir 1.16.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CimgTest.MixProject do
     [
       app: :cimg_test,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.16.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
